0a75b340eac9ac7a3a15f8686abe0ddcbf123df7
Guilherme <45200933+cguiama@users.noreply.github.com>
2024-05-30 17:03:53 -0300

    criação de repositorio

---
+++ dotnet/ExemploFundamentos/ExemploFundamentos.Common/Models/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿namespace ExemploFundamentos.Common.Models;
+
+public class Calculadora
+{
+    public void Somar(int x, int y)
+    {
+        Console.WriteLine($"A soma de {x} + {y} = {x + y}");
+    }
+    public void Subtrair(int x, int y)
+    {
+        Console.WriteLine($"A Subtração de {x} por {y} = {x - y}");
+    }
+    public void Multiplicar(int x, int y)
+    {
+        Console.WriteLine($"A Multiplicação de {x} por {y} = {x * y}");
+    }
+    public void Dividir(int x, int y)
+    {
+        Console.WriteLine($"A Divisão de {x} por {y} = {x / y}");
+    }
+    public void Modulo(int x, int y)
+    {
+        Console.WriteLine($"O Resto da divisão de {x} por {y} = {x % y}");
+    }
+    public void Potencia(int x, int y)
+    {
+        double pot = Math.Pow(x, y);
+        Console.WriteLine($"A potencia de {x} por {y} = {pot}");
+    }
+    public void Seno(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        double seno = Math.Round(Math.Sin(radiano), 4);
+        Console.WriteLine($"Seno de {angulo} = {seno}");
+    }
+    public void Coseno(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        double coseno = Math.Round(Math.Cos(radiano), 4);
+        Console.WriteLine($"Coseno de {angulo} = {coseno}");
+    }
+    public void Tangente(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        double tangente = Math.Round(Math.Tan(radiano), 4);
+        Console.WriteLine($"Tangente de {angulo} = {tangente}");
+    }
+
+    public void RaizQuadrada(double x)
+    {
+        double raiz = Math.Sqrt(x);
+        Console.WriteLine($"Raiz quadrada de {x} = {raiz}");
+    }
+}
---
+++ dotnet/ExemploFundamentos/ExemploFundamentos/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExemploFundamentos.Common.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 15);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(30);
