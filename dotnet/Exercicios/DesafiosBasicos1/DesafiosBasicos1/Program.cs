03b8de3e1972e54a730d09e825c53bdd29b69d4b
Guilherme <45200933+cguiama@users.noreply.github.com>
2024-06-29 17:33:03 -0300

    novo curso .NET

---
+++ dotnet/Exercicios/DesafiosBasicos1/DesafiosBasicos1/Program.cs
@@ -0,0 +1,21 @@
+﻿//int suavariavel = int.parse(console.readline());
+//string[] n = console.readline().split(' ');
+
+int multiplo2 = 0;
+int multiplo3 = 0;
+int multiplo4 = 0;
+int multiplo5 = 0;
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+for (int iContador = 0; iContador <= suavariavel; iContador++)
+{
+    if ((int.Parse(n[iContador]) % 2) == 0) multiplo2 = iContador;
+    if ((int.Parse(n[iContador]) % 3) == 0) multiplo3 = iContador;
+    if ((int.Parse(n[iContador]) % 4) == 0) multiplo4 = iContador;
+    if ((int.Parse(n[iContador]) % 5) == 0) multiplo5 = iContador;
+}
+
+Console.WriteLine("{0} Multiplo(s) de 2", multiplo2);
+Console.WriteLine("{0} Multiplo(s) de 3", multiplo3);
+Console.WriteLine("{0} Multiplo(s) de 4", multiplo4);
+Console.WriteLine("{0} Multiplo(s) de 5", multiplo5);
---
+++ dotnet/README.md
@@ -0,0 +1,1 @@
+# Desafio Final da formação de .NET da DIO
